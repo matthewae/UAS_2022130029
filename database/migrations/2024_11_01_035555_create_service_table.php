9132ac3ec5bc119aea2c8ce4e773a56d40a872a7
MatthewNathanael <matthewnath1508@gmail.com>
2024-11-01 11:04:48 +0700

    first0111

---
+++ database/migrations/2024_11_01_035555_create_service_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('service', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('mekanik_id')->constrained('mekanik')->onDelete('cascade');
+            $table->foreignId('pelanggan_id')->constrained('pelanggan')->onDelete('cascade');
+            $table->text('keterangan');
+            $table->decimal('biaya_service', 10, 2);
+            $table->date('tanggal');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('service');
+    }
+};
